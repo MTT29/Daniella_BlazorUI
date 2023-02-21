931bf2bf5edc0a0cc4065dadd6ed00e1aabaf586
toth tamass <toth.tamas9567@gmail.com>
2023-02-21 23:00:50 +0100

    Add project files.

---
+++ Daniella_BlazorUI/Services/LoggerService/ILogToFileService.cs
@@ -0,0 +1,13 @@
+﻿namespace Daniella_BlazorUI.Services.LoggerService
+{
+    public interface ILogToFileService
+    {
+        void LogDebug(string message);
+
+        void LogInfo(string message);
+
+        void LogError(string message);
+
+        void LogWarning(string message);
+    }
+}
---
+++ Daniella_BlazorUI/Services/LoggerService/LogToFileService.cs
@@ -0,0 +1,59 @@
+﻿namespace Daniella_BlazorUI.Services.LoggerService
+{
+    /*
+     * a fájnévtartalmazza a dátumot és vizsgáljuk meg hogy ha  a legrébbei fájl már létezik akkor ne kezdjünk újat de ha  már a dátum változott azóta akkor igen
+     *
+     *
+     *
+     *
+     *
+     */
+    public enum LogLevel
+    {
+        Debug,
+        Info,
+        Error,
+        Warning
+    }
+    public class LogToFileService : ILogToFileService
+    {
+        private readonly string defaultPath = "Log";
+
+
+        private  string logFilePath;
+
+        public LogToFileService()
+        {
+           
+        }
+
+        private void LogMessage(LogLevel level, string message)
+        {
+            logFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, defaultPath + " " + DateTime.Now.Date.ToString("yyyy'-'MM'-'dd") + ".txt");
+            var logLine = $"[{DateTime.Now}] [{level}] {message}";
+
+            File.AppendAllText(logFilePath, logLine + Environment.NewLine);
+        }
+
+        public void LogDebug(string message)
+        {
+            LogMessage(LogLevel.Debug, message);
+        }
+
+        public void LogInfo(string message)
+        {
+            LogMessage(LogLevel.Info, message);
+        }
+
+        public void LogError(string message)
+        {
+            LogMessage(LogLevel.Error, message);
+        }
+
+        public void LogWarning(string message)
+        {
+            LogMessage(LogLevel.Warning, message);
+        }
+    }
+}
+
